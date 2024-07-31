e90837fc6e617a2a9d0b4512deffe373f8ae663c
RatlavathAkhilesh03 <148054761+RatlavathAkhilesh03@users.noreply.github.com>
2024-08-01 00:13:02 +0530

    Update Policy.js

--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -13,7 +13,7 @@ const Policy = () => {
           />
         </div>
         <div className="col-md-4">
-          <p>Thankyou for Visiting EcomShopping!!!</p>
+          <p>Thankyou for Visiting QuickCart!!!</p>
           <p> We collect personal information like name, email, and payment details to process orders and improve your experience. Your data is securely stored and never shared without consent, except with necessary service providers. You have rights to access, update, or delete your information. We'll notify you, if required.</p>
           <p>Questions?? Contact us at +91-9876543210</p>
         </div>
